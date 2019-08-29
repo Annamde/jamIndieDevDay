d2575c55b4888478f825c10b24b230999fade2e1
Yaiza <yaizacastillo@users.noreply.github.com>
2019-08-29 17:56:27 +0200

    menu feo

---
+++ PillaElSueter/Assets/_Scripts/MenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject menuCanvas, tutorialCanvas;
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void EnableInstructions()
+    {
+        tutorialCanvas.SetActive(true);
+        menuCanvas.SetActive(false);
+    }
+
+    public void EnableMainMenu()
+    {
+        menuCanvas.SetActive(true);
+        tutorialCanvas.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
