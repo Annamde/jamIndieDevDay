10f7f730927b9ee533106595dfba3fc6d0c9efb9
annamde <annadiaz553@gmail.com>
2019-08-31 15:44:45 +0100

    pantalla tele

---
+++ PillaElSueter/Assets/_Scripts/GifScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GifScript : MonoBehaviour
+{
+
+    public Texture2D[] frames;
+    public float framesPorSegundo = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float index = Time.time * framesPorSegundo;
+        index = index % frames.Length;
+        GetComponent<Renderer>().material.mainTexture = frames[(int)index];
+    }
+}
